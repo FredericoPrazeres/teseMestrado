fbdb0ca8b5f87742ba687e04381ac29c4f90f38d
FredericoPrazeres <106784361+FredericoPrazeres@users.noreply.github.com>
2025-08-02 23:07:57 +0100

    The Prometheus node exporter now monitors the container

--- PROMETHEUS.md
+++ PROMETHEUS.md
@@ -6,4 +6,8 @@
 
     Verificar ligação: docker exec -it grafana ping prometheus
 
+    http://localhost:9090/targets para verificar a saude dos alvos
+
     Importar a dashboard do jenkins com o id 9964
+
+    Importar a dashboard do node exporter 1860
--- github/start.sh
+++ github/start.sh
@@ -8,6 +8,9 @@ sudo chmod 660 /var/run/docker.sock
 # Ensure runner owns the work directory
 sudo chown -R runner:runner /home/runner/_work
 
+# Start node exporter in the background
+/usr/local/bin/node_exporter --web.listen-address=":9100" &
+
 # Configure and run the GitHub Actions runner
 ./config.sh --url "$REPO_URL" --token "$RUNNER_TOKEN" --unattended --replace
 ./run.sh
