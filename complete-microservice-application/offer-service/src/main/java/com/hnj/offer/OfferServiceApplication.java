8fbffded8fadb114643fc801eea307f4cb47d06b
FredericoPrazeres <fredprazeres10@gmail.com>
2025-10-23 15:12:13 +0100

    test pipeline

--- complete-microservice-application/offer-service/src/main/java/com/hnj/offer/OfferServiceApplication.java
+++ complete-microservice-application/offer-service/src/main/java/com/hnj/offer/OfferServiceApplication.java
@@ -9,5 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class OfferServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OfferServiceApplication.class, args);
-    }   
+    }
+       
 }
