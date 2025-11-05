c88d46dc27e8693441341a871bfbf80a97c0adcf
FredericoPrazeres <fredprazeres10@gmail.com>
2025-11-05 23:46:55 +0000

    Test jenkins

--- complete-microservice-application/microservice-ui/src/main.ts
+++ complete-microservice-application/microservice-ui/src/main.ts
@@ -19,6 +19,7 @@ platformBrowserDynamic().bootstrapModule(AppModule)
 
 
 
+
   
 
 
--- jenkins/start.sh
+++ jenkins/start.sh
@@ -4,23 +4,12 @@ set -e
 sudo chown root:docker /var/run/docker.sock
 sudo chmod 660 /var/run/docker.sock
 
-if [ -d "/usr/lib/jvm/java-11-openjdk-amd64" ]; then
-    export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-elif [ -d "/usr/lib/jvm/java-11-openjdk-arm64" ]; then
-    export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-arm64
-elif [ -d "/usr/lib/jvm/java-11-openjdk" ]; then
-    export JAVA_HOME=/usr/lib/jvm/java-11-openjdk
-else
-    # Try to find Java 11 automatically
-    export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
-fi
-
-export PATH=$JAVA_HOME/bin:$PATH
-
 /usr/local/bin/cadvisor \
   --port=8081 \
   --prometheus_endpoint="/metrics" \
   --docker_only=true \
   --housekeeping_interval=30s &
 
-exec java $JAVA_OPTS -jar /usr/share/jenkins.war
+exec /usr/lib/jvm/java-1.21.0-openjdk-arm64/bin/java $JAVA_OPTS -jar /usr/share/jenkins.war
+
+
