2ac400ccf4909a1e1c8253810f3660ab181c9c07
FredericoPrazeres <106784361+FredericoPrazeres@users.noreply.github.com>
2026-06-14 11:53:37 +0100

    Update trigger-runs.ps1

--- trigger-runs.ps1
+++ trigger-runs.ps1
@@ -39,7 +39,7 @@ for ($i = 1; $i -le $NumRuns; $i++) {
     Write-Host "──────────────────────────────────────────"
 
     # Create an empty commit to trigger the pipeline
-    git commit --allow-empty -m "Windows Azure Job Test - $i/$NumRuns"
+    git commit --allow-empty -m "Windows Github Job Test - $i/$NumRuns"
 
 
     # Push to remote
